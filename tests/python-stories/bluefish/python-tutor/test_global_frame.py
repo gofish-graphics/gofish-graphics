67f64732b20eaa911cd9905ec04eaaf7f4f15e03
Josh Pollock <joshpollock1997@gmail.com>
2026-06-18 19:57:57 -0400

    test(stories): let global frame infer canvas size

--- tests/python-stories/bluefish/python-tutor/test_global_frame.py
+++ tests/python-stories/bluefish/python-tutor/test_global_frame.py
@@ -8,13 +8,10 @@ from _components import global_frame  # noqa: E402
 
 
 def story_global_frame():
-    return (
-        global_frame(
-            stack=[
-                {"variable": "c", "value": "0"},
-                {"variable": "d", "value": "0"},
-                {"variable": "x", "value": "5"},
-            ],
-        ),
-        {"w": 320, "h": 400},
+    return global_frame(
+        stack=[
+            {"variable": "c", "value": "0"},
+            {"variable": "d", "value": "0"},
+            {"variable": "x", "value": "5"},
+        ],
     )
