42ad8444968aab57b76ae2e1e8d32162ae1ffde2
Josh Pollock <joshpollock1997@gmail.com>
2026-06-18 20:45:40 -0400

    fix(parity): return global frame story tuple

--- tests/python-stories/bluefish/python-tutor/test_global_frame.py
+++ tests/python-stories/bluefish/python-tutor/test_global_frame.py
@@ -8,10 +8,13 @@ from _components import global_frame  # noqa: E402
 
 
 def story_global_frame():
-    return global_frame(
-        stack=[
-            {"variable": "c", "value": "0"},
-            {"variable": "d", "value": "0"},
-            {"variable": "x", "value": "5"},
-        ],
+    return (
+        global_frame(
+            stack=[
+                {"variable": "c", "value": "0"},
+                {"variable": "d", "value": "0"},
+                {"variable": "x", "value": "5"},
+            ],
+        ),
+        {},
     )
